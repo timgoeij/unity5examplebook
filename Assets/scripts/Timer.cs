a018dd95d5de7c0c1c36b274dc612e42b857f690
timgoeij <tim_goeijenbier@live.nl>
2016-08-20 20:33:18 +0200

    simple collection game

---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float maxTime = 60;
+
+    [SerializeField]
+    private float countdown = 0;
+    
+    // Use this for initialization
+	void Start () {
+
+        countdown = maxTime;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0)
+        {
+            coin.coinCount = 0;
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	
+	}
+}
---
+++ Assets/scripts/coin.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin : MonoBehaviour {
+
+    // Use this for initialization
+    public static int coinCount = 0;
+
+	void Start () {
+        coin.coinCount++;
+        print(coin.coinCount);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+
+    void OnDestroy()
+    {
+        coin.coinCount--;
+
+        if (coin.coinCount <= 0)
+        {
+            Destroy(GameObject.Find("Timer"));
+
+            GameObject[] fireworks = GameObject.FindGameObjectsWithTag("firework");
+
+            foreach (GameObject firework in fireworks)
+                firework.GetComponent<ParticleSystem>().Play();
+        }
+
+        print(coinCount);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            Destroy(this.gameObject);
+    }
+}
